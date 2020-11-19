e2b5deb132be6c748fbb2e13db2acb1c2212d2b3
mVince <maximevince@hotmail.com>
2020-11-19 13:54:16 +0100

    init

---
+++ app/src/main/java/com/example/exomindtest/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.exomindtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.widget.doOnTextChanged
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
